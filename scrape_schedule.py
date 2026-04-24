295b17743bdf51ca421c810fb47cb116c66bc8cf
iraeee <tjwngud123@gmail.com>
2026-04-24 20:48:09 +0900

    Delete scrape_schedule.py

--- scrape_schedule.py
+++
@@ -1,7 +0,0 @@
-import json, pandas as pd
-def main(args=None):
-    with open("hs.json", "r", encoding="utf-8") as f:
-        data = json.load(f)["data"]
-    df = pd.DataFrame(data)
-    df.to_csv("latest_data.csv", index=False)
-if __name__ == "__main__": main()
